d3e65a3e73da04912705e3699230c472fe77413c
akku2311 <140010116021@adit.ac.in>
2019-12-28 17:11:41 +0530

    changes for vs code

--- Flask_Code.py
+++ Flask_Code.py
@@ -1,11 +1,17 @@
 from flask import Flask
-
+from utility import sum_number
 app = Flask(__name__)
 
 
 # first endpoint #
 @app.route('/')
 def hello_world():
+    print(1+2)
+    input_list = list()
+    for i in range(0, 10):
+        input_list.append(i)
+    print(input_list)
+    sum_number(input_list)
     return 'Flask Dockerized'
 
 
--- utility.py
+++ utility.py
@@ -1,1 +1,6 @@
 def sum_number(args):
+    return sum(args)
+
+
+if __name__=='__main__':
+    print(sum_number((1, 2, 3)))
